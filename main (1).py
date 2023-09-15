d6a6d6efe654c16914898922623db60f88d1fc09
Sofarun0104 <144247917+Sofarun0104@users.noreply.github.com>
2023-09-15 21:29:41 +0530

    Add files via upload

--- main (1).py
+++ main (1).py
@@ -1,21 +1,29 @@
-# Leap year
+'''Implement a class called player that represents a cricket player.The player class should have a
+method called play()play which prints "The player is playing cricket.Derive two classes,Batsman and
+Bowler, from the player class.Override the play() method in each derived class to print "The batsman 
+is bating" and "The bowler is bowling ", respectively.write a program to create objects of both the 
+Batsman and Bowler classes and call the play() method for each object.'''
 
-"""
-year % 4 == 0 &
-year % 100 != 0 /
-year % 400 == 0
 
-"""
-def isLeapYear(year):
-  if (year % 4 == 0 and year % 100 != 0) or year % 400 == 0:
-    return True
-  else:
-    return False
+# Define the base class player
+class player:
+  def play(self):
+    print("The player is playing cricket.")
 
-year = int(input("Enter a year:"))
+# Define the derived class Batsman
+class Batsman(player):
+  def play(self):
+    print("The batsman is batting.")
 
-if isLeapYear(year):
-  print('{} is a leap year.'.format(year))
-else:
-  print('{} is not a leap year.'.format(year))
+# Define the derived class Bowler 
+class Bowler(player):
+  def play(self):
+    print("The bowler is bowling.")
 
+# Create objects of Batsman and Bowler classes
+batsman = Batsman()
+bowler = Bowler()
+
+# call the play() method for each object
+batsman.play()
+bowler.play()
